a67ffaf000a9c1d984b577eff32f419bd6f0e551
Rémi Pelhate <remi.pelhate@icloud.com>
2021-11-05 12:29:19 +0100

    Allow loading env vars from .env file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+import { config } from 'dotenv';
 import { Kernel, createKernel } from './kernel';
 import * as Create from './commands/create';
 import * as Cleanup from './commands/cleanup';
+
+config();
 
 const kernel: Kernel = createKernel(
     Create.COMMAND,
